dc41222dc061af2c62ab48f7e00c0eae77cda3c6
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-03-12 16:40:15 +0100

    second

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const recipe = (
+ <header>
+ <div>
+ <h2>Recette de cuisine</h2>
+ </div>
+ </header>
+
 );
+
+const rootDOMElement = document.getElementById('root');
+
+ReactDOM.render(recipe, rootDOMElement);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
