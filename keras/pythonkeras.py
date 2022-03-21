b99a16d1c3a1ac67b95465dca75b660a4c4dcbe2
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-03-21 20:34:40 +0100

    troisième

---
+++ keras/pythonkeras.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential()
+model.add(layers.Dense(2, activation="relu"))
+model.add(layers.Dense(3, activation="relu"))
+model.add(layers.Dense(4))
+
+model.summary()
